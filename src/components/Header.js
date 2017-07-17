8141b36b1fd855dd79a7d1677b5f6f248d26f3f9
Constant Oduol <constant@echomobile.org>
2017-07-17 15:26:39 +0300

    making headers resizable

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {merge} from 'lodash';
 import {DEFAULT_HEADER_WIDTH} from '../Constants';
+import Draggable from './Draggable';
 
 export default class Header extends React.Component {
 
@@ -16,20 +17,34 @@ export default class Header extends React.Component {
     this.headerResizeStopped = this.headerResizeStopped.bind(this);
     this.RESIZE_MODES = {RESIZING: "resizing", STILL: "still"};
     this.state = {
-      resizeMode: this.RESIZE_MODES.STILL
+      resizeMode: this.RESIZE_MODES.STILL,
+      left: this.props.width * 0.9,
+      previousPos: 0
     };
   }
 
-  headerResizeStarted(){
+  headerResizing(pos){
+    let {left, previousPos} = this.state
+    console.log(pos.x, previousPos);
+    let newLeft = pos.x >= previousPos ? left + 0.4 : left - 0.4;
+    this.setState({
+      resizeMode: this.RESIZE_MODES.RESIZING, 
+      left: newLeft,
+      previousPos: pos.x
+    });
+  }
+
+  headerResizeStarted(e){
+    console.log("resize started");
     this.setState({resizeMode: this.RESIZE_MODES.RESIZING});
   }
 
-  headerResizeStopped(){
-    console.log("dragging stopped");
+  headerResizeStopped(e){
+    console.log("resize stopped");
   }
 
   resizeStyle(){
-    let resizeStyle = {left: this.props.width * 0.9};
+    let resizeStyle = {left: this.state.left};
     switch(this.state.resizeMode){
       case this.RESIZE_MODES.STILL:
         return merge(styles.resize_still, resizeStyle);
@@ -44,11 +59,11 @@ export default class Header extends React.Component {
     return (
       <th style={style}>
         {this.props.title}
-        <div 
-          onDragEnd={this.headerResizeStopped}
-          onDragStart={this.headerResizeStarted}
-          className="resize" 
-          style={this.resizeStyle()}></div>
+        <Draggable onMove={pos => this.headerResizing(pos)}>
+          <div 
+            className="resize" 
+            style={this.resizeStyle()}></div>
+        </Draggable>
       </th>
     );
   }
