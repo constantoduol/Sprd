3e6546525ec25672433f906321d47184bbb497b4
constantoduol <oduolconstant@gmail.com>
2017-07-25 11:23:35 +0300

    sprd range, alt config

---
+++ src/SprdRange.js
@@ -0,0 +1,23 @@
+//renamed from Range to distinguish from window.Range
+export default class SprdRange {
+
+  constructor(startRow, startCol, stopRow, stopCol){
+    this.startRow = startRow;
+    this.startCol = startCol;
+    this.stopRow = stopRow;
+    this.stopCol = stopCol; 
+  }
+
+  isNumberCellSelected(row){
+    return this.startCol === 0 && this.stopCol === -1 && this.startRow === row;
+  }
+
+  isCellSelected(row, col){
+    return this.startRow === row && this.startCol === col && this.stopRow === row && this.stopCol === col;
+  }
+
+  isHeaderSelected(col){
+    return this.startCol === col && this.stopCol === col && this.startRow == -1 && this.stopRow === -1;
+  }
+
+}
